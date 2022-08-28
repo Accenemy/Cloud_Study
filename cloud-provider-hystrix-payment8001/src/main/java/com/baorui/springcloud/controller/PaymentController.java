6b3bf1c1eb22529fab13ba6a44ad1fc1921e9222
Accenemy <574895168@qq.com>
2022-08-28 20:43:32 +0800

    Update Hystrix

--- cloud-provider-hystrix-payment8001/src/main/java/com/baorui/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/baorui/springcloud/controller/PaymentController.java
@@ -24,18 +24,26 @@ public class PaymentController {
     private String serverPort;
 
     @GetMapping("/payment/hystrix/ok/{id}")
-    public String paymentInfo_Ok(@PathVariable("id") Integer id){
-        String result =paymentService.paymentInfo_OK(id);
-        log.info("****result:{}",result);
-        return  result;
+    public String paymentInfo_Ok(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentInfo_OK(id);
+        log.info("****result:{}", result);
+        return result;
     }
 
     @GetMapping("/payment/hystrix/timeout/{id}")
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
-        String result =paymentService.paymentInfo_TimeOut(id);
-        log.info("****result:{}",result);
-        return  result;
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentInfo_TimeOut(id);
+        log.info("****result:{}", result);
+        return result;
     }
 
+    // 服务熔断
 
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("===result={}", result);
+        return result;
+    }
 }
+
